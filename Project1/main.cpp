067dfa7acedf3ec515106875d4c52b0a6e6f69f5
Bart <98650377+Simpsons-Bart@users.noreply.github.com>
2022-10-14 13:17:36 +0800

    Add files via upload

---
+++ Project1/main.cpp
@@ -0,0 +1,19 @@
+/*求和工具：把所有输入的数加在一起*/
+#include<stdio.h>
+#include<conio.h>       /*  getchar():无法解决 一闪而过的问题，conio中_getch可以解决  */
+int main(void)
+{
+	long int fir = 0;   /*输入的数据*/
+	long int sec = 0;   /*总和*/
+	printf("这是一个求和工具，直接输入数字，按“q”退出！:");
+	while (scanf_s("%ld",&fir) == 1)  /*如果继续输入数字则进行循环 （sacnf返回值1或0） */
+	{
+		sec = fir + sec;
+		printf("输入数字，按“q”退出！:");
+	}
+	printf("和为 “ %ld ”", sec);   /*打印和*/
+	_getch();           /*防止一闪而过的问题*/
+	return 0;
+	/*如需在别的电脑上运行，则需要设置以下内容
+	项目→属性→配置属性→高级→MFC的使用→在静态库中使用MFC*/
+}
